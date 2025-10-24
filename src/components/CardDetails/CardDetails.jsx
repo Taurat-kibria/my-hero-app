5a09317e5aec870edfd9b290da792839abc3daec
Taurat-kibria <tauratkibria9@gmail.com>
2025-10-24 15:06:56 +0600

    app detals loaded with data

--- src/components/CardDetails/CardDetails.jsx
+++ src/components/CardDetails/CardDetails.jsx
@@ -1,6 +1,7 @@
 
 import { useLoaderData } from 'react-router';
-import demo from '../../assets/demo-app (1).webp'
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
+
 import downloadImg from '../../assets/icon-downloads.png'
 import ratingImg from '../../assets/icon-ratings.png'
 import reviewImg from '../../assets/icon-review.png'
@@ -10,50 +11,79 @@ import reviewImg from '../../assets/icon-review.png'
 const CardDetails = () => {
     // const data = useContext(DataContext)
     // console.log(data)
-    const data = useLoaderData()
-    console.log(data)
+    const datas = useLoaderData()
+    const chartData = datas.ratings
+    console.log(chartData)
+
+    // "image": "https://images.unsplash.com/photo-1519397150501-4d9d97b3918d",
+    // "title": "SpeakEase",
+    // "companyName": "LinguaTech",
+    // "id": 14,
+    // "description": "SpeakEase makes language learning fun and interactive—live conversation practice, game-based lessons, and instant feedback help you confidently speak a new language in weeks, not months.",
+    // "size": 68,
+    // "reviews": 3300,
+    // "ratingAvg": 4.7,
+    // "downloads": 240000,
+    
     return (
         <div className='bg-[#f5f5f5] px-[50px] pt-20 '>
-            <div className='flex gap-8   '>
+            <div className='flex gap-20   '>
                 <div >
-                    <img className='h-[350px] w-[350px]' src={demo} alt="" />
+                    <img className='h-[350px] w-[350px]' src={datas.image} alt="" />
                 </div>
                 <div>
                     <h1 className='font-semibold text-4xl '>
-                        SmPlan:ToDo List with Reminder
+                        {datas.title}
                     </h1 >
                     <p className='font-semibold text-xl text-[#647584]
                          mt-3.5 mb-8 border-b border-dashed pb-8 '>
-                        Developed by <span className='text-[#7a44e9] '>llll</span>
+                        Developed by <span className='text-[#7a44e9] '>{datas.companyName}</span>
                     </p>
-                    <div className='flex justify-around mb-11'>
+                    <div className='flex gap-10 mb-11'>
                         <div className='space-y-0.5 '>
-                            <img src={downloadImg}  />
+                            <img src={downloadImg} />
                             <p className='text-[#647584]'>Downloads</p>
-                            <h1 className='font-bold text-4xl '>8M</h1>
+                            <h1 className='font-bold text-4xl '>{datas.downloads/1000000}M</h1>
                         </div>
                         <div className='space-y-0.5 '>
-                            <img src={ratingImg}  />
+                            <img src={ratingImg} />
                             <p className='text-[#647584]'>Average Ratings</p>
-                            <h1 className='font-bold text-4xl '>4.5</h1>
+                            <h1 className='font-bold text-4xl '>{datas.ratingAvg}</h1>
                         </div>
                         <div className='space-y-0.5 '>
-                            <img src={reviewImg}  />
+                            <img src={reviewImg} />
                             <p className='text-[#647584]'>Total Reviews</p>
-                            <h1 className='font-bold text-4xl '>54K</h1>
+                            <h1 className='font-bold text-4xl '>{datas.reviews/1000}K</h1>
                         </div>
                     </div>
                     <button
                         className='btn bg-[#00d390] rounded-lg
                          text-white  text-lg py-3 px-5 '
-                        >Install Now (291MB)
+                    >Install Now ({datas.size}MB)
 
                     </button>
                 </div>
             </div>
-            
 
-            
+            <div className='mt-5 mb-20 '>
+                <h1 className='font-semibold text-2xl mb-3 '>Ratings</h1>
+                <BarChart height={600} width={800} data={chartData} layout='horizontal'>
+                    <XAxis dataKey='name'>
+
+                    </XAxis>
+                    <YAxis ></YAxis>
+                    <Bar dataKey='count' fill='#ff8811' barSize={30}></Bar>
+
+                </BarChart>
+            </div>
+
+            <div>
+                <h1 className='font-semibold text-2xl mb-3 '>Description</h1>
+                <p className='text-xl text-[#a1abb3] '>
+                    {datas.description }
+                </p>
+            </div>
+
         </div>
     );
 };
